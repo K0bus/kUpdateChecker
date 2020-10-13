3476e4fe40e5e0ef641e626fdc35df8d3123bf04
K0bus <contact@k0bus.fr>
2020-10-14 01:24:06 +0200

    Add main class

---
+++ src/main/java/fr/k0bus/kUpdateChecker.java
@@ -0,0 +1,14 @@
+package fr.k0bus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class kUpdateChecker extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Loading K0bus Updater !");
+    }
+    @Override
+    public void onDisable() {
+
+    }
+}
